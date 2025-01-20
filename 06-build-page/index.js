3be5ad5e78ccd48e6b5f93920620eb542bc13d33
May <abeillee@bk.ru>
2025-01-20 18:30:13 +0300

    feat: add solution for 06-build-page

---
+++ 06-build-page/index.js
@@ -0,0 +1,151 @@
+const fs = require('fs');
+const path = require('path');
+
+const projectDistPath = path.join(__dirname, 'project-dist');
+const stylesCopyPath = path.join(projectDistPath, 'style.css');
+const assetsFolder = path.join(__dirname, 'assets');
+const assetsCopyPath = path.join(projectDistPath, 'assets');
+const stylesPath = path.join(__dirname, 'styles');
+const componentsPath = path.join(__dirname, 'components');
+const templatePath = path.join(__dirname, 'template.html');
+
+
+fs.rm(projectDistPath, { recursive: true, force: true }, (err) => { 
+    if (err)
+        console.error('Error while removing a directory', err);
+
+
+    fs.mkdir(projectDistPath, { recursive: true }, (err) => {
+        if (err) {
+            console.error('Error while creating a directory', err);
+            return;
+        }
+
+
+        fs.readFile(templatePath, 'utf-8', (err, templateData) => {
+            if (err) {
+                console.error('Error while reading tepmlate.html', err);
+            }
+
+   
+            fs.readdir(componentsPath, (err, files) => {
+                if (err) {
+                    console.error('Error while reading components folder');
+                }
+          
+                const components = {};
+                let count1 = 0;
+
+                files.forEach((component) => {
+                    const nameOfComponent = path.basename(component, path.extname(component));
+
+                    fs.readFile(path.join(componentsPath, component), 'utf-8', (err, data) => {
+                        if (err) {
+                            console.error('Error while reading a component', err);
+                        }
+                        components[nameOfComponent] = data;
+                        count1++;
+
+                        if (count1 === files.length) {
+                            let result = templateData;
+
+                          
+                            for (const [name, content] of Object.entries(components)) {
+                                const tag = `{{${name}}}`;
+                                result = result.replace(new RegExp(tag, 'g'), content);
+                            }
+
+                            fs.writeFile(path.join(projectDistPath, 'index.html'), result, (err) => {
+                                if (err) {
+                                    console.error("File index.html wasn't created", err);
+                                } else {
+                                    console.log('File index.html was created successfully');
+                                }
+                            });
+                        }
+                    });
+                });
+            });
+        });
+
+        //создание файла style.css в project dist
+        fs.readdir(stylesPath, { withFileTypes: true }, (err, files) => {
+            if (err) {
+                console.error('Error while reading the directory');
+                return;
+            }
+
+            const writeStream = fs.createWriteStream(stylesCopyPath);
+            let count = files.length;
+
+            files.forEach((file) => {
+                if (file.isFile() && path.extname(file.name) === '.css') {
+                    const filePath = path.join(stylesPath, file.name);
+
+                    const readStream = fs.createReadStream(filePath, 'utf-8');
+                    readStream.on("data", (chunk) => writeStream.write(chunk + '\n'));
+
+                    readStream.on("error", (error) => {
+                        console.log("Error", error.message);
+                        count--;
+                    });
+
+                    readStream.on('end', () => {
+                        count--;
+                        if (count === 0) {
+                            writeStream.end();
+                            console.log('The styles merging was completed')
+                        }
+                    });
+
+                } else {
+                    count--;
+                }
+            });
+        });
+
+        //копирование файлов из assets
+        function copyDirectory(src, destinationPath) {
+            fs.mkdir(destinationPath, { recursive: true }, (err) => {
+                if (err) {
+                    console.error('Error while creating a copy of folder assets', err);
+                }
+
+                fs.readdir(src, (err, files) => {
+                    let count = 0;
+
+                    if (err) {
+                        console.error('Errow while reading the directory', err);
+                        return;
+                    } else {
+                        files.forEach((file) => {
+                            const srcFile = path.join(src, file);
+                            const destinationFile = path.join(destinationPath, file);
+
+                            fs.stat(srcFile, (err, stat) => {
+                                if (err) {
+                                    console.error('Error while reading stat', err);
+                                }
+
+                                if (stat.isDirectory()) {
+                                    copyDirectory(srcFile, destinationFile);
+                                } else {
+                                    fs.copyFile(srcFile, destinationFile, (err) => {
+                                        if (err) {
+                                            console.error('Errow while copying a file', err);
+                                        }
+                                        count++;
+                                        if (count === files.length) {
+                                            console.error('The folder was copied successfully');
+                                        }
+                                    });
+                                }
+                            });
+                        });
+                    }
+                });
+            });
+        }
+        copyDirectory(assetsFolder, assetsCopyPath);
+    });
+});
